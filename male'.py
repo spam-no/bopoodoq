f3ea170dce0c1a7726c2e66a5ad9631b6bdb89ce
spam-no <85016293+spam-no@users.noreply.github.com>
2021-05-29 17:59:42 +0300

    Add files via upload
    here OFCF, (open for coding files), do anything with this.

---
+++ male'.py
@@ -0,0 +1,4 @@
+import tkinter.messagebox as msb
+import tkinter
+tkinter.Tk().withdraw()
+msb.showerror('HABALLA HABULLA HAUBLA','YOU\'RE FUCKED, SMOL CHICKE,,,,N!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1')
